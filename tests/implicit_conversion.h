9a2af41887374a3defde1ac837160eb6076ef22f
Johann Klähn <johann@jklaehn.de>
2019-12-15 16:58:56 +0100

    Add test case for implicit conversion with by-reference constructor

--- tests/implicit_conversion.h
+++ tests/implicit_conversion.h
@@ -8,6 +8,7 @@ struct GENPYBIND(visible) Three {};
 
 // Implicit conversions have to be annotated explicitly (the absence of the `explicit`
 // keyword is not enough).
+
 struct GENPYBIND(visible) Value {
   Value(One one);
   explicit Value(Two two);
@@ -19,4 +20,20 @@ struct GENPYBIND(visible) Value {
 };
 
 GENPYBIND(visible)
-int test_value(Value const &value);
+int test_value(Value value);
+
+// Implicit conversion should also work for reference types.
+
+struct GENPYBIND(visible) Number {
+  Number(int value) : value(value) {}
+  int value;
+};
+
+struct GENPYBIND(visible) ByReference {
+  /*implicit*/ ByReference(Number const &number) GENPYBIND(implicit_conversion)
+      : value(number.value) {}
+  int value;
+};
+
+GENPYBIND(visible)
+int test_by_reference(ByReference instance);
--- tests/implicit_conversion_test.py
+++ tests/implicit_conversion_test.py
@@ -10,11 +10,15 @@ def test_from_class_explicit_without_annotation():
         m.test_value(m.Two())
 
 def test_from_class_implicit_with_annotation():
-    m.test_value(m.Three())
+    assert m.test_value(m.Three()) == 3
 
 def test_from_pod_explicit_without_annotation():
     with pytest.raises(TypeError, match="incompatible function arguments"):
         m.test_value(2.0)
 
 def test_from_pod_implicit_with_annotation():
-    m.test_value(42)
+    assert m.test_value(42) == 42
+
+def test_by_reference():
+    number = m.Number(123)
+    assert m.test_by_reference(number) == 123
